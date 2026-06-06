1a56daae3e7fc4782292aa7077d79dbc589551a3
Dan Hagman <hagman.dan@gmail.com>
2026-06-06 14:31:03 -0700

    fix: allow firebasestorage.googleapis.com as next/image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: process.cwd(),
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+    ],
+  },
 }
 
 export default withSentryConfig(nextConfig, {
